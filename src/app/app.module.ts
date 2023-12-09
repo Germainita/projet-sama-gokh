702db8447fb7e9cafc92efe06da3aa608de6a7de
khadidiatou6c <khadidiatoucisse2018@outlook.fr>
2023-12-09 16:25:26 +0000

    gestion annonces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './composants/side-bar/side-bar.component';
 import { DashboardComponent } from './composants/adminCommune/dashboard/dashboard.component';
+import { GestionProjetMunicipalComponent } from './composants/adminCommune/gestion-projet-municipal/gestion-projet-municipal.component';
+import { GestionAnnoncesComponent } from './composants/adminCommune/gestion-annonces/gestion-annonces.component';
+import { DetailsAnnonceComponent } from './composants/adminCommune/details-annonce/details-annonce.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
-    DashboardComponent
+    DashboardComponent,
+    GestionProjetMunicipalComponent,
+    GestionAnnoncesComponent,
+    DetailsAnnonceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/composants/adminCommune/gestion-projet-municipal/gestion-projet-municipal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-projet-municipal',
+  templateUrl: './gestion-projet-municipal.component.html',
+  styleUrls: ['./gestion-projet-municipal.component.css']
+})
+export class GestionProjetMunicipalComponent {
+
+
+  
+}
