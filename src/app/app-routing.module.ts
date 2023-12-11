f4c894617e26fb5989b1d358fe7c4d4e75f9830c
Fatou Diop SOLY <solyfatoudiop@gmail.com>
2023-12-11 13:32:58 +0100

    sowlymodiff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './composants/superAdmin/accueil/accueil.component';
+import { UtilisateurComponent } from './composants/superAdmin/utilisateur/utilisateur.component';
+import { AnnonceComponent } from './composants/superAdmin/annonce/annonce.component';
+import { CommuneComponent } from './composants/superAdmin/commune/commune.component';
+import { ProjetComponent } from './composants/superAdmin/projet/projet.component';
+import { RolesComponent } from './composants/superAdmin/roles/roles.component';
+import { VilleComponent } from './composants/superAdmin/ville/ville.component';
+import { GestionProjetComponent } from './composants/superAdmin/gestion-projet/gestion-projet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'dashboard', component: DashboardComponent }, // Route pour le tableau de bord
+  { path: 'accueil', component: AccueilComponent}, // Route vers le  dashboard du  superAdmin
+  { path: 'utilisateur', component:UtilisateurComponent},
+  { path: 'annonce', component:AnnonceComponent},
+  { path: 'commune', component:CommuneComponent},
+  { path: 'projet', component:ProjetComponent},
+  { path: 'roles', component:RolesComponent},
+  { path: 'ville', component:VilleComponent},
+  { path: 'gestion-projet', component:GestionProjetComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,40 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './composants/side-bar/side-bar.component';
 import { DashboardComponent } from './composants/adminCommune/dashboard/dashboard.component';
+import { AccueilComponent } from './composants/superAdmin/accueil/accueil.component';
+import { UtilisateurComponent } from './composants/superAdmin/utilisateur/utilisateur.component';
+import { ProjetComponent } from './composants/superAdmin/projet/projet.component';
+import { AnnonceComponent } from './composants/superAdmin/annonce/annonce.component';
+import { RolesComponent } from './composants/superAdmin/roles/roles.component';
+import { VilleComponent } from './composants/superAdmin/ville/ville.component';
+import { CommuneComponent } from './composants/superAdmin/commune/commune.component';
+import { Routes } from '@angular/router';
+import { GestionProjetComponent } from './composants/superAdmin/gestion-projet/gestion-projet.component';
+
+
+
+
+
 
 @NgModule({
+  
   declarations: [
     AppComponent,
     SideBarComponent,
-    DashboardComponent
+    DashboardComponent,
+    AccueilComponent,
+    UtilisateurComponent,
+    ProjetComponent,
+    AnnonceComponent,
+    RolesComponent,
+    VilleComponent,
+    CommuneComponent,
+    GestionProjetComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
