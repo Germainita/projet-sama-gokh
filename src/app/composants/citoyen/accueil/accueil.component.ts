7d82d69906282204040eb1edf8f7118f28c2fd4f
Germainita <dioufgermainedaba@gamil.com>
2023-12-11 16:49:16 +0000

    modif

--- src/app/composants/citoyen/accueil/accueil.component.ts
+++ src/app/composants/citoyen/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Projet } from 'src/app/models/projet';
 
 @Component({
   selector: 'app-accueil',
@@ -8,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 export class AccueilComponent implements OnInit{
   // Déclaration des variables 
   // un objet de type projet 
-  
+  projet =  new Projet;
+
+  tabProjet: Projet[]  = [];
 
   ngOnInit(): void {
+  }
+
+  image1: any;
+  // Methode pour soumettre un projet 
+  soumettreProjet(){
+    console.log(this.projet);
+    const fileUrl: string = URL.createObjectURL(this.projet.image);
+    console.log(fileUrl)
     
   }
+
+  // afficher l'image 
+  image: string = "";
+  // Methode pour uploader le fichier image 
+  uploadFile(event: Event) {
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    console.log(fileList);
+    if (fileList) {
+      const selectedFile = fileList[0];
+      console.log(typeof(selectedFile))
+      if (selectedFile) {
+        if (selectedFile.type.match('image.*')) {
+            this.image = (URL.createObjectURL(selectedFile));
+        } 
+      }
+    }
+  }
+
+
 }
