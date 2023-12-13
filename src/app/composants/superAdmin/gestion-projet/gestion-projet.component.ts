a5c7c84ce0aec5d06d1c6aff22671691f0997399
Fatou Diop SOLY <solyfatoudiop@gmail.com>
2023-12-13 09:24:39 +0100

    sowlymodiff

--- src/app/composants/superAdmin/gestion-projet/gestion-projet.component.ts
+++ src/app/composants/superAdmin/gestion-projet/gestion-projet.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EtatProjetService } from 'src/app/services/etatProjet-service';
+import { TypeProjetService } from 'src/app/services/typeProjet-service';
+
 
 @Component({
   selector: 'app-gestion-projet',
@@ -11,9 +13,20 @@ export class GestionProjetComponent implements OnInit {
   etatProjets: any[] = []; // Remplacez any[] par le type de vos données d'état de projet
   typeProjets: any[] = []; // Remplacez any[] par le type de vos données de type de projet
   
-  constructor(private etatProjetService: EtatProjetService) {};
+  constructor(
+    private etatProjetService: EtatProjetService,
+    private typeProjetService: TypeProjetService
+  ) {};
   
   ngOnInit(): void {
-    
+    // Récupération des états de projets
+    this.etatProjetService.getAlls().subscribe((data: any[]) => {
+      this.etatProjets = data;
+    });
+
+    // Récupération des types de projets
+    this.typeProjetService.getAlls().subscribe((data: any[]) => {
+      this.typeProjets = data;
+    });
   }
 }
